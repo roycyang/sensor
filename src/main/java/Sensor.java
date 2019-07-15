c2d2347fccd94282b31f7d29c1bb79e6c8588019
Roy Yang <roy.yang@giantmachines.com>
2019-07-15 16:17:38 -0400

    Adding in sensor answers

--- src/main/java/Sensor.java
+++ src/main/java/Sensor.java
@@ -1,23 +1,68 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Sensor {
 
   private int lower;
   private int higher;
   private int[] buffer;
+  private int[] output;
 
   Sensor(int lower, int higher, int[] buffer) {
-
+    this.lower = lower;
+    this.higher = higher;
+    this.buffer = buffer;
   }
 
+
   public double findMean() {
-    return 0;
+    return Arrays.stream(this.getOutput()).average().orElse(Double.NaN);
   }
 
   public double findMedian() {
-    return 0;
+    int[] a = this.getOutput();
+    int n = a.length;
+
+    // Even
+    if (n % 2 != 0) {
+      return (double)a[n / 2];
+    }
+
+    return (double)(a[(n - 1) / 2] + a[n / 2]) / 2.0;
   }
 
   public int findMode() {
-    return 0;
+    // Creating a HashMap
+    Map<Integer, Integer> modeMapping = new HashMap<>();
+
+    int[] output = this.getOutput();
+    int highest = 0;
+    int mode = output[0];
+    int i;
+    int count;
+
+    System.out.println("MODE");
+
+    // iterating over an array
+    for (i = 0; i < output.length; i++) {
+      int key = output[i];
+
+      if (modeMapping.containsKey(key)) {
+        count = modeMapping.get(key);
+        modeMapping.put(key, count + 1);
+      } else {
+        modeMapping.put(key, 1);
+        count = 1;
+      }
+
+      if (count >= highest) {
+        highest = count;
+        mode = key;
+      }
+    }
+
+    return mode;
   }
 
 
@@ -33,4 +78,21 @@ public class Sensor {
     return buffer;
   }
 
+  public int[] getOutput() {
+    int[] output = Arrays.stream(this.buffer).filter(number -> number >= this.lower && number <= this.higher).toArray();
+    Arrays.sort(output);
+    return output;
+  }
+
+  public String toString() {
+    String strOfInts = Arrays
+            .stream(this.getOutput())
+            .mapToObj(String::valueOf)
+            .reduce((a, b) -> a.concat(",").concat(b))
+            .get();
+
+    return strOfInts;
+  }
 }
+
+
